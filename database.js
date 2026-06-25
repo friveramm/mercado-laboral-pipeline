671c632363e1ce6e69bc831b809d1ccd7b70dd7e
friveramm <felipealejandro.rm@gmail.com>
2026-06-24 21:45:44 -0400

    feat: agrega ssl para conexion con postgresql via neon tech

--- database.js
+++ database.js
@@ -1,18 +1,23 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// Configura el pool de conexiones usando variables de entorno
+// Configura el pool de conexiones usando variables de entorno y activa SSL
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    // Configuración SSL conectarse a PostgreSQL en la nube de Neon Tech
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
 });
 
 // Verifica la conexión a la base de datos
 pool.on('connect', () => {
-    console.log('Conexion establecida con PostgreSQL.');
+    console.log('Conexion segura SSL establecida con PostgreSQL en la Nube.');
 });
 
 // Exporta el pool para ser utilizado en otros modulos
